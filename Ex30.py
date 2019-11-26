f5ac52649b640e7b97d37a5b6091fd85be19358b
eudjx <56890938+eudjx@users.noreply.github.com>
2019-11-26 13:42:56 -0500

    Ex30.py

--- Ex30.py
+++ Ex30.py
@@ -1,18 +1,51 @@
-#Código tomado de mi repositorio, ejercicio 29
+#Código tomado del repositorio del profesor Jaime Forero, ejercicio 29
 
-import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.loadtxt("Ex29.dat")
-plt.figure(figsize=(20,5))
-plt.subplot(1,2,1)
-plt.imshow(data/np.max(data[:,0]))
-plt.colorbar()
-plt.subplot(1,2,2)
-plt.plot(data[:,0], data[:,1]/np.max(data[:,1]))
-plt.title("Diffusive equation")
-plt.xlabel("t")
-plt.ylabel("Diffusion")
-plt.savefig("diff.png")
-plt.show()
+
+
+def grafica(datafile, plotfile, plotlabel):
+    data = np.loadtxt(datafile)
+    
+    
+    plt.figure(figsize=(14,4))
+    
+    plt.subplot(1,3,1)
+    s = np.shape(data)
+    print(s)
+    n_x = s[1]
+    n_t = s[0]
+    plt.imshow(data, extent=[-1,1,1,0], aspect=2.0)
+    plt.colorbar(label="$\psi$")
+    plt.xlabel("Posicion")
+    plt.ylabel("Tiempo")
+    plt.title(plotlabel)
+
+
+    plt.subplot(1,3,2)
+    x = np.linspace(-1,1,n_x)
+    delta_t = 1.0/n_t
+    for i in range(n_t):
+        if i%(n_t//9) == 0:
+            plt.plot(x, data[i,:], alpha=(i+1)/n_t, color='black', label="t={:.02f}".format(i*delta_t))
+    plt.legend(loc=1)
+    plt.xlabel("Posicion")
+    plt.ylabel("$\psi$")
+            
+    plt.subplot(1,3,3)
+    t = np.linspace(0,1,n_t)
+    plt.plot(t, data[:,n_x//2], alpha=i/n_t, color='black')
+    plt.xlabel("Tiempo")
+    plt.ylabel("$\psi$(x=0)")
+
+
+    plt.savefig(plotfile, bbox_inches='tight')
+
+grafica("evolve_30_450.dat", "evolve_A.png", "$N_x=30$ ,  $N_{tc}$")
+
+grafica("evolve_31_450.dat", "evolve_B.png", "$N_x=31$ ,  $N_{tc}$")
+
+grafica("evolve_29_450.dat", "evolve_C.png", "$N_x=29$ ,  $N_{tc}$")
+
+grafica("evolve_30_440.dat", "evolve_D.png", "$N_x=30$ ,  $N_{tc}-10$")
